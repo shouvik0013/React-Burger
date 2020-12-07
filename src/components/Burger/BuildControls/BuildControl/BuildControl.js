12c88b9ca8f4f46805f65c317607951b4d7c7397
Shouvik Mandal <shouvik0013@gmail.com>
2020-12-07 10:11:57 +0530

    Bare minimum BuildControls added to BurgerBuilder

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './BuildControl.css';
 
 
+
 const buildControl = (props) => {
     return(
         <div className={classes.BuildControl}>
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    {label: "Salad", type:"salad"},
+    {label: "Bacon", type:"bacon"},
+    {label: "Cheese", type:"cheese"},
+    {label: "Meat", type:"meat"},
+];
 
 const buildControls = ( props ) => {
+    const transformedControls = controls.map((elms, index) => {
+        return(
+            <BuildControl key={elms.label} igName={elms.label}/>
+        );
+    });
     return(
         <div className={classes.BuildControls}>
-            {/* igName is Ingredient Name */}
-            <div>{props.igName}</div>
-            <button>Less</button>
-            <button>More</button>
+            {transformedControls}
         </div>
     );
 };
